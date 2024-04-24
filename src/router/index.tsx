b77c2406396c251da92c9e8bf8f6e8ea4e4475ad
Clay04 <s22210410@student.unklab.ac.id>
2024-04-24 11:52:33 +0800

    adding router

---
+++ src/router/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Router = () => {
+  return (
+    <View>
+      <Text>Router</Text>
+    </View>
+  )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
