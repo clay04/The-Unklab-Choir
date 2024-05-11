6ca8bcc534cb8528d604cca1f1deeb8f419f6932
Clay04 <s22210410@student.unklab.ac.id>
2024-05-11 23:51:34 +0800

    adding configuration firebase in this app in config directory

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/router';
-
 import React from 'react';
+import './src/config/Firebase';
 
 const App = () => {
   return (
