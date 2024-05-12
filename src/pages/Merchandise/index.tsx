e5afaeb1dd25f1d25f80ea72ac1cc51bba9e54f4
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-12 19:34:49 +0700

    import into merchandise

--- src/pages/Merchandise/index.tsx
+++ src/pages/Merchandise/index.tsx
@@ -1,5 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {Gap, Header} from '../../components';
+import {lanyardpurple, lanyardred, shirt, tumbler} from '../../assets';
 
 const Merchandise = () => {
   return (
