dab0210dee463bab71957ed929754ac8d5d55e4c
Clay04 <s22210410@student.unklab.ac.id>
2024-05-13 00:36:10 +0800

    Slicing Members Page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {AlbumsPages, Menu, Songs, SongsPlay} from '../pages';
+import {AlbumsPages, Altos, Basses, Coach, Conductor, Members, MembersMenu, Menu, Officers, ProfileMembers, Songs, SongsPlay, Sopranos, Tenors} from '../pages';
 
 
 const Stack = createNativeStackNavigator();
@@ -10,22 +10,46 @@ const Router = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen 
-        name="Menu"
-        component={Menu}
+        name="MembersMenu"
+        component={MembersMenu}
         options={{headerShown: false}}/>
+    <Stack.Screen 
+      name="Members"
+      component={Members}
+      options={{headerShown: false}}/>
       <Stack.Screen 
-        name="Songs"
-        component={Songs}
-        options={{headerShown: false}}/>
+      name="Sopranos"
+      component={Sopranos}
+      options={{headerShown: false}}/>
       <Stack.Screen 
-        name="Albums"
-        component={AlbumsPages}
-        options={{headerShown: false}}/>
+      name="Altos"
+      component={Altos}
+      options={{headerShown: false}}/>
       <Stack.Screen 
-        name="SongsPlay"
-        component={SongsPlay}
-        options={{headerShown: false}}/>
-    </Stack.Navigator>
+      name="Tenors"
+      component={Tenors}
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="Basses"
+      component={Basses}
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="Officers"
+      component={Officers}
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="Conductor"
+      component={Conductor}
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="Coach"
+      component={Coach}
+      options={{headerShown: false}}/>
+      <Stack.Screen 
+      name="ProfileMembers"
+      component={ProfileMembers}
+      options={{headerShown: false}}/>
+  </Stack.Navigator>
   );
 };
 
