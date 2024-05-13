fdf16532cf5b895f6b210be7eec032294c71d616
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-14 00:16:48 +0700

    add Schedule in index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Booking from './Booking';
 import Merchandise from './Merchandise';
 import Payment from './Payment';
+import Schedule from './Schedule';
 
 export {
     Booking,
     Merchandise,
     Payment,
+    Schedule
 };
