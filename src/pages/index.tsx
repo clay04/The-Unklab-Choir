d3c13bcc6e90721d2e4a0d52245abdcbe769ea6c
koyanagiyuki <katilikyuki@gmail.com>
2024-05-13 15:38:52 +0700

    adding schedule page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Booking from './Booking';
 import Merchandise from './Merchandise';
 import Audition from './Audition';
+import Schedule from './Schedule';
 
 export {
     Booking,
     Merchandise,
-    Audition
+    Audition,
+    Schedule,
 };
