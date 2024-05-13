cf8d95a2e3b5f1664e0bdba0e4f69d1aa38b0564
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-13 07:47:28 +0700

    add Booking

--- src/components/atom/index.tsx
+++
@@ -1,6 +0,0 @@
-import Button from "./Button";
-import Gap from "./Gap";
-import Search from "./Search";
-import MenuButton from "./MenuButton";
-
-export {Button, Gap, Search, MenuButton};
--- src/components/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from './molekul';
-export * from './atom';
