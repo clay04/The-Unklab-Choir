e006f9e238444324dc884c9c0fd892f81deb80a1
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-14 00:12:17 +0700

    add and import Schedule

---
+++ src/pages/Schedule/index.tsx
@@ -0,0 +1,4 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {Gap, Header} from '../../components';
