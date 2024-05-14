d7b54eb4fbc3f62c22c0399714dfebba9d568530
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-14 08:39:15 +0700

    add new file performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import Booking from './Booking';
 import Merchandise from './Merchandise';
 import Payment from './Payment';
 import Schedule from './Schedule';
+import Perfromance from './Performances';
 
 export {
     Booking,
     Merchandise,
     Payment,
-    Schedule
+    Schedule,
+    Perfromance
 };
