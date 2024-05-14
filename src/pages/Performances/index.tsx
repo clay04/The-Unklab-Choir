0c21906bab6ab6784f2287655b2f807d4fa1e92d
Jonathan Kieron Medellu <145462579+Jmedelllu@users.noreply.github.com>
2024-05-14 08:54:44 +0700

    add import data to performance

---
+++ src/pages/Performances/index.tsx
@@ -0,0 +1,5 @@
+import {ScrollView, StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import {List, Gap, Header, Search} from '../../components';
+import {performance, logoblack} from '../../assets';
